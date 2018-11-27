3d1f40121a0c608216532c2215e25e7f43c8852e
Unknown <jerome.papalie@gmail.com>
2018-11-28 01:25:59 +0800

    created skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoverpageComponent } from './coverpage/coverpage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoverpageComponent
   ],
   imports: [
     BrowserModule
