96edfe57efe1ff4ad3a43dc86339b8f717b03200
Jerome Papalie <jerome.papalie@gmail.com>
2018-12-01 00:52:35 +0800

    show hide components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  showFlag = true;
+  templateCompName: string;
+
   constructor() { }
 
+  toggleComponent(value) {
+    switch (value) {
+      case 1:
+        this.templateCompName = "story";
+        this.showFlag = false;
+        break;
+      case 2:
+        this.templateCompName = "work";
+        this.showFlag = false;
+        break;
+      case 3:
+        this.templateCompName = "certs";
+        this.showFlag = false;
+        break;
+      case 4: 
+        this.templateCompName = "contact";
+        this.showFlag = false;
+        break;
+      default:
+        break;
+    }
+    
+  }
+  
   ngOnInit() {
   }
 
